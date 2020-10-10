16dc4306655fe0655c8357a0e5f3706e93ea5913
ZeusSura <Alejandrogzh@gmail.com>
2020-10-10 13:59:24 -0500

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { Fragment, useEffect, useState } from "react";
+import Header from "./Componnets/Header";
+import Formulario from "./Componnets/Formulario";
+import ListadoNoticias from "./Componnets/ListadoNoticias";
 function App() {
+
+  const[categoria,setCategoria] = useState('')
+  const[noticias,setNoticias] =useState([])
+
+  useEffect(()=>{
+    const consultarAPI= async()=>
+    {
+      const url = `https://newsapi.org/v2/top-headlines?country=mx&category=${categoria}&apiKey=4396d990f40e4f7e86091f57a7a886a9`
+
+      const response =  await fetch(url)
+      if(!response.ok)return;
+      const data = await response.json()
+      setNoticias(data.articles)
+    }
+    consultarAPI()
+  },[categoria])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <Header titulo="Ultimas noticias en Mexico" />
+      <div className="container white">
+        <Formulario
+        setCategoria={setCategoria}
+        />
+        <ListadoNoticias
+        noticias={noticias}/>
+      </div>
+    </Fragment>
   );
 }
 
